79ff9a22c8364f93648910d33ca2f403659e5e8b
James Doyley <jdoyley@gmail.com>
2016-01-05 16:27:36 +0000

    Add to precompile list in production.rb
    Why:
    
    To pick up additional files
    
    This change addresses the need by:
    
    N/A

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ HullBid2013::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ['print.css', 'static_tree.js']
+  config.assets.precompile += ['print.css', 'tiny_mce/tiny_mce.js', 'static_tree.js', 'manticore.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
