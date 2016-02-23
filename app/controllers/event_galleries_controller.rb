95f45c52cda0911ea25b9f1d90b5e2e258234581
Paul <pauloptimised@users.noreply.github.com>
2016-02-23 16:53:43 +0000

    Add town and postcode

--- app/controllers/event_galleries_controller.rb
+++ app/controllers/event_galleries_controller.rb
@@ -1,11 +1,11 @@
 class EventGalleriesController < ApplicationController
 
   def index
-    @event_galleries = EventGallery.where(:display => true).order(:position)
+    @event_galleries = EventGallery.where(:display => true).order("created_at desc")
   end
 
   def show
-    @event_galleries = EventGallery.where(:display => true).order(:position)
+    @event_galleries = EventGallery.where(:display => true).order("created_at desc")
     @event_gallery = EventGallery.find(params[:id])
     @business_highlight = BusinessHighlight.where(:display => true).order("date desc").first
   end
